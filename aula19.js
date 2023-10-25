d41a104ea05e12a54f1c1e82b53d8af56584b006
Felipe Freitas <felipefreitas210891@gmail.com>
2023-10-25 20:01:20 -0300

    aula 19

---
+++ aula19.js
@@ -0,0 +1,34 @@
+// let laranja = {
+//     Cor: "laranja",
+//     Preco: "R$ 5,00",
+//     Tipo: "Fruta"
+// }
+// console.log(laranja)
+
+let maça = {
+    nome: "maça",
+    cor:"vermelha",
+    preço: "R$ 1,50",
+    tipo: "Frutas",
+}
+let banana = {
+    nome: "banana",
+    cor:"amarela",
+    preço: "R$ 2,50",
+    tipo: "Frutas",
+}
+let abacate = {
+    nome: "abacate",
+    cor:"verde",
+    preço: "R$ 7,00",
+    tipo: "Frutas",
+}
+let repolho = {
+    nome: "repolho",
+    cor:"branco",
+    preço: "R$ 3,50",
+    tipo: "verduras",
+}
+let Alimentos = [maça,banana,abacate,repolho]
+console.log(Alimentos);
+console.log(Alimentos[2].nome)
