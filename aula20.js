975b2b9b131344bfd860a4e53abc22a2e27f5a07
Felipe Freitas <felipefreitas210891@gmail.com>
2023-10-25 20:49:43 -0300

    aula 20

---
+++ aula20.js
@@ -0,0 +1,8 @@
+// let body = document.getElementById("body")
+// console.log(body)
+
+let fundoazul = document.getElementsByClassName("fundoazul")
+console.log(fundoazul[1])
+
+let body = document.getElementById("body")
+body.innerHTML = "<h2> Felipe Freitas !!!</h2> <p> Lorem </P>"
