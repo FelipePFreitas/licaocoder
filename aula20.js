ab055b33ba8704140871c50faaf6a775576f2cea
Felipe Freitas <felipefreitas210891@gmail.com>
2023-10-25 21:36:09 -0300

    Aula 20

--- aula20.js
+++ aula20.js
@@ -1,8 +1,15 @@
-// let body = document.getElementById("body")
-// console.log(body)
+let body = document.getElementById("body")
+console.log(body)
 
 let fundoazul = document.getElementsByClassName("fundoazul")
 console.log(fundoazul[1])
 
 let body = document.getElementById("body")
 body.innerHTML = "<h2> Felipe Freitas !!!</h2> <p> Lorem </P>"
+
+let botao1 = document.getElementById ("botao1");
+botao1.addEventListener("click", acaoClique);
+
+function acaoClique (){
+    alert("Felipe")
+}
